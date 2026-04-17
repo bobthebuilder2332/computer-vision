948e3162b3f6b7ecb957a04ffaac18477c6a4c42
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-17 13:15:45 -0400

    Load and show image in new window with OpenCv

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import cv2
 
-print ("Hello, world!")
+img = cv2.imread('image.png') # Import the image file "image.png"
+cv2.imshow('Image', img) # Display the image in a window titled "Image"
+cv2.waitKey(0) # Wait until user presses key "0" to close the window
+cv2.destroyAllWindows() # Close all OpenCV windows
