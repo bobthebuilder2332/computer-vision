7903371ce758a49f9b6a3b1a289578d327808bee
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-20 11:59:12 -0400

    Play a video in a window frame by frame

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import cv2
 
 
-img = cv2.imread('image.png')
+vid = cv2.VideoCapture('video.mp4')
 
-cv2.imshow("Image", img) # Creates window with img but doesn't render until waitKey()
+# Loop for playing the video
+while True:
+    success, img = vid.read() # Reads a frame from the video and stores it in img, while storing success as a boolean indicating if the read was successful
+    cv2.imshow("Video", img)
 
-cv2.waitKey(0) # Wait indefinitely until a key is pressed
+    """
+    Breaks out of loop and closes program if the 'q' key is pressed
 
-cv2.destroyAllWindows() # Close all OpenCV windows
+    waitKey(1) waits for 1 millisecond while returning the ASCII code of any key pressed
+    & is a bitwise AND operator that returns 1 if both are 1 and 0 otherwise
+    0xFF is the hexadecimal literal for 255, used for masking
+        Together they "cut" everything out of the 32 bit return value, leaving only the last 8 bits which represent the ASCII code of the key pressed
+    ord('q') returns the Unicode value for the character 'q'
+    """
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+cv2.destroyAllWindows() # Clean up and close all OpenCV windows
