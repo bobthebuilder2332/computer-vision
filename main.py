aebbbdf7d2366f84e8e1cb85ce72c5b7fac5725a
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-22 10:08:38 -0400

    Image resizing and cropping

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import numpy as np
 
 
 img = cv2.imread('image.jpg')
+imgResize = cv2.resize(img, (300, 200))
+imgCrop = img[0:200, 200:500] # Crops image [y1:y2, x1:x2]
 
-kernel = np.ones((7,7), np.uint8) # Creates a kernel for dilation; the (5, 5) is the size of the kernel, and np.uint8 is the data type of the kernel elements
+print(img.shape) # Prints shape of image (height, width, channels[colors])
+print(imgResize.shape)
+print(imgCrop.shape)
 
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converts image to grayscale; remmeber opencv uses BGR format instead of RGB
-imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0) # Blurs the image using a Gaussian filter; the (7, 7) is the kernel size (must be odd), and 0 is the standard deviation in X and Y direction
-imgCanny = cv2.Canny(imgBlur, 100, 100) # Detects edges in the image using the Canny edge detection algorithm; the two 100s are the lower and upper thresholds for the hysteresis procedure
-imgDilated = cv2.dilate(imgCanny, kernel, iterations=1) # Dilates the edges in the image; the (7, 7) is the kernel size for dilation, and iterations=1 means it will be applied once
-imgEroded = cv2.erode(imgDilated, kernel, iterations=1) # Erodes the dilated image; the (7, 7) is the kernel size for erosion, and iterations=1 means it will be applied once
+cv2.imshow('image', img)
+cv2.imshow('image resize', imgResize)
+cv2.imshow('image crop', imgCrop)
 
-cv2.imshow('Gray Image', imgGray)
-cv2.imshow('Blurred Image', imgBlur)
-cv2.imshow('Canny Image', imgCanny)
-cv2.imshow('Dilated Image', imgDilated)
-cv2.imshow('Eroded Image', imgEroded)
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
