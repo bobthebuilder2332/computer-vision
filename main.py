78b64627db254838463ff81babc300aae32957fd
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-23 10:19:27 -0400

    Function for alternate method of joining images

--- main.py
+++ main.py
@@ -3,12 +3,43 @@ import numpy as np
 
 img = cv2.imread('image.jpg')
 
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
-# Stacking has to be done on images with thes same channels (i.e. RGB doesn't work with BGR or grayscale)
-hor = np.hstack((img, img)) # Stacks matrices horizontally (column-wise)
-ver = np.vstack((img, img)) # Stacks matrices vertically (row-wise)
+stack = stackImages(.5, ([img, img, img], [img, img, img]))
 
-cv2.imshow('Image', hor)
+hor = np.hstack((img, img, img))
+ver = np.vstack((hor, hor))
+
+cv2.imshow('Image', stack)
 cv2.imshow('Image2', ver)
 
 cv2.waitKey(0)
