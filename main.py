5e2e4faa39529d84483eed42958ebf9e51d0b624
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-23 10:15:55 -0400

    Join images using numpy matrix stacking

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image2.jpg')
+img = cv2.imread('image.jpg')
 
-w, h = 250, 350
 
-points1 = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]]) # Points of the image to be transformed
-points2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]]) # Points of the output image (the rectangle to which the original points will be mapped)
+# Stacking has to be done on images with thes same channels (i.e. RGB doesn't work with BGR or grayscale)
+hor = np.hstack((img, img)) # Stacks matrices horizontally (column-wise)
+ver = np.vstack((img, img)) # Stacks matrices vertically (row-wise)
 
-matrix = cv2.getPerspectiveTransform(points1, points2) # Calculate the perspective transformation matrix based on the corresponding points
-
-imgOut = cv2.warpPerspective(img, matrix, (w, h)) # Apply the perspective transformation to the original image using the calculated matrix and specify the size of the output image (w, h)
-
-cv2.imshow('Image', img)
-cv2.imshow('ImageOut', imgOut)
+cv2.imshow('Image', hor)
+cv2.imshow('Image2', ver)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
