47f1838e25e1968b211f5ead79b7a22a20b194d6
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-26 22:42:27 -0400

    Color detection

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image.jpg')
+img = cv2.imread('image.png')
+
+def empty(a):
+    pass
 
 def stackImages(scale,imgArray):
     rows = len(imgArray)
@@ -34,13 +37,34 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-stack = stackImages(.5, ([img, img, img], [img, img, img]))
+cv2.namedWindow('TrackBars')
+cv2.resizeWindow('TrackBars', 640, 240)
+cv2.createTrackbar('Hue Min', 'TrackBars', 0, 179, empty)
+cv2.createTrackbar('Hue Max', 'TrackBars', 179, 179, empty)
+cv2.createTrackbar('Sat Min', 'TrackBars', 0, 255, empty)
+cv2.createTrackbar('Sat Max', 'TrackBars', 255, 255, empty)
+cv2.createTrackbar('Val Min', 'TrackBars', 0, 255, empty)
+cv2.createTrackbar('Val Max', 'TrackBars', 255, 255, empty)
 
-hor = np.hstack((img, img, img))
-ver = np.vstack((hor, hor))
+imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # HSV used because better for color detectoin in different lighting conditions
 
-cv2.imshow('Image', stack)
-cv2.imshow('Image2', ver)
+while True:
+    h_min = cv2.getTrackbarPos('Hue Min', 'TrackBars')
+    h_max = cv2.getTrackbarPos('Hue Max', 'TrackBars')
+    s_min = cv2.getTrackbarPos('Sat Min', 'TrackBars')
+    s_max = cv2.getTrackbarPos('Sat Max', 'TrackBars')
+    v_min = cv2.getTrackbarPos('Val Min', 'TrackBars')
+    v_max = cv2.getTrackbarPos('Val Max', 'TrackBars')
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    imgStack = stackImages(.5, ([img, imgHSV], [mask, imgResult]))
+    cv2.imshow('Stacked Images', imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows() # Outside loop to avoid weird erros with insistent windows
