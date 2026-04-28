a3d87d45d04434fc093aa29f41aa1911beae4eb4
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-28 17:21:32 -0400

    License plate detection

--- main.py
+++ main.py
@@ -1,116 +1,41 @@
 import cv2
-import numpy as np
 
-widthImg = 540
-heightImg = 640
+frameWidth = 640
+frameHeight = 480
+nPlateCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
+minArea = 200
+color = (255,0,255)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture("video2.mp4")
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
 cap.set(10,150)
-
-
-def preProcessing(img):
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-    imgCanny = cv2.Canny(imgBlur,200,200)
-    kernel = np.ones((5,5))
-    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThres = cv2.erode(imgDial,kernel,iterations=1)
-    return imgThres
-
-def getContours(img):
-    biggest = np.array([])
-    maxArea = 0
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area>5000:
-            #cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt,True)
-            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
-            if area >maxArea and len(approx) == 4:
-                biggest = approx
-                maxArea = area
-    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
-    return biggest
-
-def reorder (myPoints): # Reorder points in the order of top-left, top-right, bottom-left, bottom-right
-    myPoints = myPoints.reshape((4,2))
-    myPointsNew = np.zeros((4,1,2),np.int32)
-    add = myPoints.sum(1)
-    #print("add", add)
-    myPointsNew[0] = myPoints[np.argmin(add)] # Top-left point has the smallest sum of coordinates
-    myPointsNew[3] = myPoints[np.argmax(add)] # Bottom-right point has the largest sum of coordinates
-    diff = np.diff(myPoints,axis=1)
-    myPointsNew[1]= myPoints[np.argmin(diff)] # Top-right point has the smallest difference of coordinates
-    myPointsNew[2] = myPoints[np.argmax(diff)] # Bottom-left point has the largest difference of coordinates
-    #print("NewPoints",myPointsNew)
-    return myPointsNew
-
-def getWarp(img,biggest):
-    biggest = reorder(biggest)
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
-
-    imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
-    imgCropped = cv2.resize(imgCropped,(widthImg,heightImg))
-
-    return imgCropped
-
-
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+count = 0
 
 while True:
     success, img = cap.read()
-    img = cv2.resize(img,(widthImg,heightImg))
-    imgContour = img.copy()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    numberPlates = nPlateCascade.detectMultiScale(imgGray, 1.1, 10)
+    for (x, y, w, h) in numberPlates:
+        area = w*h
+        if area >minArea:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(img,"Number Plate",(x,y-5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,2)
+            imgRoi = img[y:y+h,x:x+w]
+            cv2.imshow("ROI", imgRoi)
 
-    imgThres = preProcessing(img)
-    biggest = getContours(imgThres)
-    if biggest.size !=0:
-        imgWarped=getWarp(img,biggest)
-        # imageArray = ([img,imgThres],
-        #           [imgContour,imgWarped])
-        imageArray = ([imgContour, imgWarped])
-        cv2.imshow("ImageWarped", imgWarped)
-    else:
-        # imageArray = ([img, imgThres],
-        #               [img, img])
-        imageArray = ([imgContour, img])
+    cv2.imshow("Result", img)
 
-    stackedImages = stackImages(0.6,imageArray)
-    cv2.imshow("WorkFlow", stackedImages)
-
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg",imgRoi)
+        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
+        cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_DUPLEX,
+                    2,(0,0,255),2)
+        cv2.imshow("Result",img)
+        cv2.waitKey(500)
+        count +=1
+    
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 cap.release()
