1e864b2486d98e4af99aea85a1536334365bb389
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-27 22:24:14 -0400

    Color tracker
    Isolates and tracks colors in a video feed and tags them with colored dots

--- main.py
+++ main.py
@@ -1,48 +1,67 @@
 import cv2
 import numpy as np
-
 frameWidth = 640
 frameHeight = 480
-video = cv2.VideoCapture(0)
-video.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
-video.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
 
+myColors = [[167,196,216,178,255,255],
+            [61,200,235,74,222,255],
+            [104,239,184,109,253,214],]
+myColorValues = [[51,153,255],          ## BGR
+                 [255,0,255],
+                 [0,255,0],]
 
-def empty(a):
-    pass
+myPoints =  []  ## [x , y , colorId ]
 
+def findColor(img,myColors,myColorValues):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    count = 0
+    newPoints=[]
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV,lower,upper)
+        x,y=getContours(mask)
+        cv2.circle(imgResult,(x,y),15,myColorValues[count],cv2.FILLED)
+        if x!=0 and y!=0:
+            newPoints.append([x,y,count])
+        count +=1
+        #cv2.imshow(str(color[0]),mask)
+    return newPoints
 
-cv2.namedWindow("HSV")
-cv2.resizeWindow("HSV", 640, 240)
-cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
-cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
-cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
-cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
-cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
-cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>500:
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            x, y, w, h = cv2.boundingRect(approx)
+    return x+w//2,y
+
+def drawOnCanvas(myPoints,myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+
 
 while True:
+    success, img = cap.read()
+    imgResult = img.copy()
+    newPoints = findColor(img, myColors,myColorValues)
+    if len(newPoints)!=0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints)!=0:
+        drawOnCanvas(myPoints,myColorValues)
 
-    _, frame = video.read()
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
-    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
-    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
-    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
-    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
-    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
-
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-    mask = cv2.inRange(frameHSV, lower, upper)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    
-    hStack = np.hstack([frame, mask, result])
-    cv2.imshow('Horizontal Stacking', hStack)
-
+    cv2.imshow("Result", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
-video.release()
+cap.release()
 cv2.destroyAllWindows()
